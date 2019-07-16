233f3820a04d6f5dbcc2590eae4a019a5f6654e2
Nicola Sartorato <97nico.s@gmail.com>
2019-07-16 19:02:51 +0200

    Add some paths

--- src/commons.py
+++ src/commons.py
@@ -14,10 +14,16 @@ CSV_GLOVE_PATH = CSV_PATH + "glove/"
 CSV_ST_CONCEPT_NAMES_GLOVE_PATH = CSV_GLOVE_PATH + "symptom_tree/concept_names/"
 CSV_ST_WORDS_GLOVE_PATH = CSV_GLOVE_PATH + "symptom_tree/words/"
 
+CSV_ST_MEDICAL_WORDS_GLOVE_PATH = CSV_GLOVE_PATH + "symptom_tree/medical_words/"
+CSV_ST_MEDICAL_TERMS_FILE = CSV_PATH + "symptom_tree/medical_terms.csv"
+
 TXT_PATH = BASEDIR + "data/txt/"
 TXT_GLOVE_PATH = TXT_PATH + "glove/"
 
+DATASET_CV_FOLDER = BASEDIR + "dataset/cross_validation/"
+DATASET_TRAINING_FOLDER = BASEDIR + "dataset/testing/"
 
+STANFORD_NLP_RESOURCES_PATH = BASEDIR + "data/stanfordnlp_resources/"
 
 ADMITTED_REPRESENTATIONS = ["glove"]
 

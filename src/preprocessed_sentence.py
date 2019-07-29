3836fa61e0086580567c6091821b0f0a03370726
Nicola Sartorato <97nico.s@gmail.com>
2019-07-29 17:12:48 +0200

    Change name to Sentence class

---
+++ src/preprocessed_sentence.py
@@ -0,0 +1,54 @@
+import commons as COM
+import pandas as pd
+from tokenizer import Tokenizer
+from body_part import BodyPart
+
+
+class PreprocessedSentence:
+  
+  
+  def __init__(self, sentence_text):
+    bpt = BodyPartTagger()
+    self.text = sentence_text
+    self.__body_parts = bpt.get_body_parts_in_sentence(sentence_text)
+    
+  
+  def has_body_parts(self):
+    return len(self.__body_parts) != 0
+  
+  
+  def get_body_parts(self):
+    return self.__body_parts
+  
+  
+  
+class BodyPartTagger:
+  
+  
+  def __init__(self):
+    #carica file e lista di body parts
+    self.body_parts = []
+    body_parts_df = pd.read_csv(COM.CSV_BODY_PARTS_FILE)
+    for _, row in body_parts_df.iterrows():
+      bp = BodyPart(row["ID"],
+                    row["NAME"],
+                    row["REF_TO"],
+                    row["IS_EXTERNAL"],
+                    row["NOT_SPECIFICALLY_LOCATED"],
+                    row["ONE_WORD"])
+      self.body_parts.append(bp)
+  
+  
+  def get_body_parts_in_sentence(self, sentence):
+    tokenizer = Tokenizer()
+    tokens = tokenizer.tokenize(sentence)
+    
+    found_body_parts = []
+    for body_part in self.body_parts:
+      for token in tokens:
+        if token.text in body_part.names:
+          body_part.exact_word = token.text
+          found_body_parts.append(body_part)
+    
+    return found_body_parts
+  
